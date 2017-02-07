e4e899f72a39721b9e54ee08af03b9dfca0002d4
gnosygnu <gnosygnu@gmail.com>
2017-02-07 13:49:12 -0500

    DownloadCentral: When moving items between tables, add item to correct spot in table, not at bottom

--- 400_xowa/src/gplx/xowa/addons/bldrs/centrals/tasks/Xobc_task_itm.java
+++ 400_xowa/src/gplx/xowa/addons/bldrs/centrals/tasks/Xobc_task_itm.java
@@ -19,11 +19,14 @@ package gplx.xowa.addons.bldrs.centrals.tasks; import gplx.*; import gplx.xowa.*
 import gplx.core.gfobjs.*;
 import gplx.xowa.addons.bldrs.centrals.cmds.*; import gplx.xowa.addons.bldrs.centrals.steps.*;
 public class Xobc_task_itm implements gplx.CompareAble {
+	private final    int task_sort;
 	public Xobc_task_itm(int task_id, int task_seqn, int step_count, String task_key, String task_name) {
-		this.task_id = task_id; this.task_seqn = task_seqn; 
+		this.task_id = task_id;
+		this.task_seqn = task_seqn;
 		this.step_count = step_count;
 		this.task_key = task_key;
 		this.task_name = task_name;
+		this.task_sort = task_seqn;	// set task_sort to task_seqn; note that task_sort needs to be memorialized b/c "work" changes task_seqn to add-order
 	}
 	public int				Task_id()		{return task_id;} private final    int task_id;
 	public String			Task_key()		{return task_key;} private final    String task_key;
@@ -36,11 +39,11 @@ public class Xobc_task_itm implements gplx.CompareAble {
 	public void				Task_status_(byte v) {task_status = v;}		// called when task moves from init -> working -> suspended -> done
 	public void				Task_seqn_(int v) {this.task_seqn = v;}		// called when task is init'd from db, or added to list
 	public int				compareTo(Object obj) {Xobc_task_itm comp = (Xobc_task_itm)obj; return Int_.Compare(task_seqn, comp.task_seqn);}
-
 	public Gfobj_nde Save_to(Gfobj_nde nde) {
 		nde.Add_int	("task_id"				, task_id);
 		nde.Add_str	("task_name"			, task_name);
 		nde.Add_byte("task_status"			, task_status);
+		nde.Add_int ("task_sort"			, task_sort);
 		if (step != null) {
 			step.Save_to(nde.New_nde("step"));
 		}
