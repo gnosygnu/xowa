1ba13dd5f7bb1dd2bddce663c1219d8e044c56cc
gnosygnu <gnosygnu@gmail.com>
2017-02-21 15:21:28 -0500

    Sql_exec: Change page alias

--- 400_xowa/src/gplx/xowa/addons/apps/maints/sql_execs/specials/Xosql_exec_special.java
+++ 400_xowa/src/gplx/xowa/addons/apps/maints/sql_execs/specials/Xosql_exec_special.java
@@ -30,5 +30,5 @@ public class Xosql_exec_special implements Xow_special_page {
 	Xosql_exec_special(Xow_special_meta special__meta) {this.special__meta = special__meta;}
 	public Xow_special_meta Special__meta()		{return special__meta;} private final    Xow_special_meta special__meta;
 	public Xow_special_page Special__clone()	{return this;}
-	public static final    Xow_special_page Prototype = new Xosql_exec_special(Xow_special_meta.New_xo("XowaSqlExec", "SQL Exec", "XowaSql"));
+	public static final    Xow_special_page Prototype = new Xosql_exec_special(Xow_special_meta.New_xo("XowaSql", "SQL Exec"));
 }
