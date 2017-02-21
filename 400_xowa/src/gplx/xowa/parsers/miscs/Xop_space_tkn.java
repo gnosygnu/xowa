1179e2848fe1129343b2699b191ed1b345ea65d0
gnosygnu <gnosygnu@gmail.com>
2017-02-21 15:45:17 -0500

    Developer: Change license to GPLv3 / Apache2 in source code headers

--- 400_xowa/src/gplx/xowa/parsers/miscs/Xop_space_tkn.java
+++ 400_xowa/src/gplx/xowa/parsers/miscs/Xop_space_tkn.java
@@ -1,19 +1,17 @@
 /*
 XOWA: the XOWA Offline Wiki Application
-Copyright (C) 2012 gnosygnu@gmail.com
+Copyright (C) 2012-2017 gnosygnu@gmail.com
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+XOWA is licensed under the terms of the General Public License (GPL) Version 3,
+or alternatively under the terms of the Apache License Version 2.0.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+You may use XOWA according to either of these licenses as is most appropriate
+for your project on a case-by-case basis.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+The terms of each license can be found in the source code repository:
+
+GPLv3 License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-GPLv3.txt
+Apache License: https://github.com/gnosygnu/xowa/blob/master/LICENSE-APACHE2.txt
 */
 package gplx.xowa.parsers.miscs; import gplx.*; import gplx.xowa.*; import gplx.xowa.parsers.*;
 import gplx.core.btries.*; import gplx.xowa.parsers.tmpls.*;
