8653e665cc83e936c4152446e6763d8b5f0aadc1
gnosygnu <gnosygnu@gmail.com>
2019-11-05 06:04:19 -0500

    HTTP_Server: Add 2 line breaks to redirect response for proxy servers [#600]

--- 400_xowa/src/gplx/xowa/apps/servers/http/Http_server_wkr.java
+++ 400_xowa/src/gplx/xowa/apps/servers/http/Http_server_wkr.java
@@ -192,6 +192,7 @@ class Xosrv_http_wkr_ {
 				, Rsp__location
 				, redirect
 				, Byte_ascii.Nl_bry
+				, Byte_ascii.Nl_bry // proxy servers like nginx require 2 line breaks; ISSUE#:600; DATE:2019-11-05
 				)
 			);
 		} catch (Exception err) {
